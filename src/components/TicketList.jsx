7ac474f942a2aac7a35df9a93abea575a8f960be
Jessie <jess.bell@me.com>
2018-01-16 22:23:43 -0800

    Add code following along with homework

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Ticket from './Ticket';
+
+function TicketList(){
+  return(
+    <Ticket
+      location="3A"
+      names="Thato and Haley"
+      issue="Firebase not saving records"
+    />
+  );
+
+}
+
+export default TicketList;
